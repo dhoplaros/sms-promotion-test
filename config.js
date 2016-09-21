f742c1550a85823c5e2a93c3c2a0c92089ec5cd0
Demetris Hoplaros <dhoplaros@gmail.com>
2016-09-22 02:47:20 +0300

    Mods...

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ var cfg = {};
 cfg.port = process.env.PORT || 8081;
 
 //set the following to own API keys - these should either be loaded from a file, or from env variables
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID || "AC262d22ca851753c3257a89a5faa2eb4c";
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN || "185856e67ec6b92e9495b1a0f0d964c4";
-cfg.sendingNumber = process.env.TWILIO_NUMBER || "+17194284440";
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID || "account id set here"; //SET ACCOUNT ID
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN || "auth token here"; //SET TOKEN
+cfg.sendingNumber = process.env.TWILIO_NUMBER || "phone...";  //SET PHONE
 
 // Export configuration object
 module.exports = cfg;
